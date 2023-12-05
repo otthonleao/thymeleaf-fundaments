0d6b0a4f36e5911aeb178e5ee74b289785b9cfad
Otthon Leão <otthonleao@hotmail.com>
2023-12-05 13:24:24 -0400

    feat: add feature with `th:text="${ ... }`

--- src/main/java/dev/otthon/springclients/controller/HomeController.java
+++ src/main/java/dev/otthon/springclients/controller/HomeController.java
@@ -1,19 +1,30 @@
 package dev.otthon.springclients.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(ModelMap model) {
+        //( [nome da variável que será chamada na view], [o que será exibido na view por meio da variável])
+        model.addAttribute("boasVindas", "Bem-vindo ao curso de Spring com Thymeleaf");
+
+        List<String> aulas = new ArrayList<>();
+        aulas.add("Aula 01 - Introdução");
+        aulas.add("Aula 02 - Template Engines");
+        aulas.add("Aula 03 - Arquivos Estáticos");
+        model.addAttribute("aulasList", aulas);
+
         return "home";
     }
 
